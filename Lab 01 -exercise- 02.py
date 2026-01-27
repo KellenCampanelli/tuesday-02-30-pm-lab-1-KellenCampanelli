c5e62bede13d0fd54893537df6b6843d9b5405e5
KellenCampanelli <kellen@ku.edu>
2026-01-27 15:37:33 -0600

    Create Lab 01 -exercise- 02.py

---
+++ Lab 01 -exercise- 02.py
@@ -0,0 +1,20 @@
+
+'''
+Author: Kellen Campanelli
+KUID: 3200503
+Date: 1/27/2026
+Lab: lab 01
+Last modified: 1/27/2026
+Purpose: Has my name and hobbies
+'''
+
+
+
+print("My name is Kellen Campanelli.")
+print("I am an Computer Engineering major.")
+print("My hobbies are:")
+print("\tGaming")
+print("\tWatching movies")
+print("\tCoding")
+print("\tSleeping")
+print("Goodbye")
